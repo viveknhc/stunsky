28ca957760692c4155854cc9c40f096cb3d510d7
viveknhc <viveknhc@gmail.com>
2022-12-21 16:41:18 +0530

    service

--- stunsky/settings.py
+++ stunsky/settings.py
@@ -128,3 +128,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+STATIC_URL = "/static/"
+STATIC_FILE_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = ((BASE_DIR / "static"),)
+STATIC_ROOT = BASE_DIR / "assets"
